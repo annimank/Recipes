0e7719f4335b8c4d72ae74fc35ddc568f2b65b43
annimank <mankki.anni@gmail.com>
2023-09-16 11:09:02 +0300

    First

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Alert, Image } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  const getRepositories = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+      .then(response => response.json())
+      .then(data => {
+        setRepositories(data.meals);
+      })
+      .catch(error => {
+        Alert.alert('Error', error);
+      });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={{ fontSize: 18, width: 200 }}
+        placeholder='keyword'
+        value={keyword}
+        onChangeText={text => setKeyword(text)}
+      />
+      <Button title="Find" onPress={getRepositories} />
+      <FlatList
+        keyExtractor={(item) => item.idMeal.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "bold" }}>{item.strMeal}</Text>
+            <Image
+              source={{ uri: `https://www.themealdb.com/images/media/meals/${item.idMeal}.jpg/preview` }}
+              style={{ width: 100, height: 100 }}
+              onError={(error) => {
+                console.error('Image loading error:', error.nativeEvent.error);
+              }}
+            />
+            <Text style={{ fontSize: 16 }}>{item.strInstructions}</Text>
+          </View>
+        )}
+        data={repositories}
+      />
     </View>
   );
 }
@@ -16,5 +52,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 30,
   },
 });
